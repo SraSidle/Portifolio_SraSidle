bce1fb367d378383f0ab097a6bb8d888844e8b59
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-06-04 23:11:13 -0300

    setTimeout funcionando no lugar certo

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ function stars(){
         let h = Math.random() * 100;
 
         star.style.left = x + 'px';
-        star.style.width = 1 + 'px';
-        star.style.height = 60 + 'px';
+        star.style.width = 2 + 'px';
+        star.style.height = 40 + 'px';
         star.style.animationDuration = duration + 's';
         
         scene.appendChild(star);
         i++;
 
-        for(i=0; i<count; i++){
-            
-        }
+     
+            setTimeout(() => { star.style.background = "white"}, 0500);
+            setTimeout(() => { star.style.background+ "yellow"}, 1000); // setTime funcionando, só falta fazer a animação direito
+            setTimeout(() => { star.style.background= "cyan"}, 1000);
+            setTimeout(() => { star.style.background = "green"}, 1500);
+            setTimeout(() => { star.style.background= "#321450"}, 2000); 
 
-        setTimeout(() => { 'i'.style.background = "linear-gradient(blue, #f887ff"}, 2000);
-        setTimeout(() => { star.style.height=20 + "px"}, 2000);
     }
 }
 
 stars();
-
